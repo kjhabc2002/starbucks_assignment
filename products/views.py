0da00daf48ab591e2c1a331aafa007f74f1073ca
juhyun <kjhabc2002@naver.com>
2021-12-04 10:21:11 +0900

    Create : modeling & migrate complete

--- products/views.py
+++ products/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views import View
+from django.http import JsonResponse
+
+from products.models import (
+    Menu,
+    Category,
+    Product,
+    Image,
+    Allergy,
+)
+
+class MenuView(View):
+    def get(self, request):
+        menu = list(Menu.objects.values())
+        
+        return JsonResponse({'data':Menu}, status=200)
