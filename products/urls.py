ef679a73f23be9879587c2078c094d2af7314e97
juhyun <kjhabc2002@naver.com>
2021-12-04 11:26:44 +0900

    [CREATE] Menu view작성완료

---
+++ products/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views      import MenuView
+
+urlpatterns = [
+    path('/menu', MenuView.as_view())
+]
--- products/views.py
+++ products/views.py
@@ -14,5 +14,23 @@ from products.models import (
 class MenuView(View):
     def get(self, request):
         menu = list(Menu.objects.values())
+            
+        return JsonResponse({'data':menu}, status=200)
+    
+    def post(self, request):
+        data = json.loads(request.body)
+        menu_name = data.get('name', None)
+        print(menu_name)
+        if not menu_name:
+            return JsonResponse({'MESSAGE': 'KEY_ERROR'}, status=400)
         
-        return JsonResponse({'data':Menu}, status=200)
+        Menu.objects.create(name=menu_name)
+        return JsonResponse({'MESSAGE' : 'SUCCESS'}, status=200)
+    
+class CategoryView(View):
+    def get(self, request):
+        category = list(Category.objects.values())
+            
+        return JsonResponse({'data':category}, status=200)
+    
+    def post(self, request):
