3c737593d947ea5ba8ec53d787aa765d68ab1944
juhyun <kjhabc2002@naver.com>
2021-12-06 08:03:11 +0900

    [CREATE] Category view작성완료

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views      import MenuView
+from .views      import MenuView,CategoryView
 
 urlpatterns = [
-    path('/menu', MenuView.as_view())
+    path('/menu', MenuView.as_view()),
+    path('/category', CategoryView.as_view())  
 ]
